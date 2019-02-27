49e8938cec606c6e9fd42816f452080b54e68bdf
Noah Serrault <25417@verona.k12.wi.us>
2019-02-27 14:14:01 -0600

    Add project files.

---
+++ Widgets/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Widgets
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LBLTitle = New System.Windows.Forms.Label()
+        Me.LBL1 = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(-2, -2)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1006, 533)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.LBL1)
+        Me.TabPage1.Controls.Add(Me.LBLTitle)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(998, 504)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(192, 71)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(381, 235)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 63)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LBLTitle
+        '
+        Me.LBLTitle.AutoSize = True
+        Me.LBLTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.8!)
+        Me.LBLTitle.Location = New System.Drawing.Point(36, 37)
+        Me.LBLTitle.Name = "LBLTitle"
+        Me.LBLTitle.Size = New System.Drawing.Size(255, 31)
+        Me.LBLTitle.TabIndex = 1
+        Me.LBLTitle.Text = "Call Cost Calculator"
+        '
+        'LBL1
+        '
+        Me.LBL1.AutoSize = True
+        Me.LBL1.Location = New System.Drawing.Point(118, 109)
+        Me.LBL1.Name = "LBL1"
+        Me.LBL1.Size = New System.Drawing.Size(163, 17)
+        Me.LBL1.TabIndex = 2
+        Me.LBL1.Text = "Length of Call in Minutes"
+        '
+        'Widgets
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(1003, 525)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Widgets"
+        Me.Text = "Widgets"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents LBL1 As Label
+    Friend WithEvents LBLTitle As Label
+End Class
---
+++ Widgets/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Widgets
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Dim sum, num1 As Decimal
+
+    End Sub
+
+End Class
